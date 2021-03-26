2a87cde8195c52feaa6abfc45d7e87a9da42c839
pjjmaster <pavanjoshi029@gmail.com>
2021-03-26 14:26:43 +0530

    Added approach using HashMap

--- src/com/practice/array/ValidAnagrams.java
+++ src/com/practice/array/ValidAnagrams.java
@@ -30,6 +30,27 @@ public class ValidAnagrams {
 
 		return true;
 	}
+	
+	public boolean isAnagram(String s, String t) {
+		HashMap<Character,Integer> smap=new HashMap<>();
+		
+		int sl=s.length();
+		int tl=t.length();
+		
+		if(sl!=tl){
+			return false;
+		}
+		
+		for(int i=0;i<sl;i++){
+		    smap.put(s.charAt(i),smap.getOrDefault(s.charAt(i),0)+1);
+		    smap.put(t.charAt(i),smap.getOrDefault(t.charAt(i),0)-1);
+		}
+		for(char c:smap.keySet()){
+		    if(smap.get(c)!=0){return false;}
+		}
+
+		return true;
+    	}
 
 	public static void main(String[] args) {
 		String s1 = "anagram";
