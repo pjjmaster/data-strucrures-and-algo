42a83ff9aab653890215bcf80e8b0d8d9a641cb0
pjjmaster <pavanjoshi029@gmail.com>
2022-04-07 21:54:30 +0530

    Update MatrixConnectedCellsOf1s.java

--- src/com/practice/array/MatrixConnectedCellsOf1s.java
+++ src/com/practice/array/MatrixConnectedCellsOf1s.java
@@ -1,41 +1,64 @@
 package com.practice.array;
 
-// Finding the length of cells of 1s(regions) in a matrix of 0s and 1s
+class MatrixConnectedCellsOf1s {
 
-public class MatrixConnectedCellsOf1s {
-  
-  public static int getMaximumLengthOfConnectedCellsof1s(int[][] array) {
+  public static int connectedCell(int[][] array) {
+    // Write your code here
+    int maxConnectedCells = 0;
 
-    int maxConnectedCells= 0;
-
-    for(int row=0; i<array.length; i++) {
-      for(int cloumn=0; j<array[row].length; j++) {
+    for (int row = 0; row < array.length; row++) {
+      for (int column = 0; column < array[row].length; column++) {
         //If element is 1 then calculate count in adjacent region
-        if(array[row][column] == 1) {
-          cellsCount = getConnectedCountsRegion(array, row, column);
-          maxConncctedCells = Math.mac(cellsCount, maxConnectedCells)
+        if (array[row][column] == 1) {
+          int cellsCount = getConnectedCountsRegion(array, row, column);
+          maxConnectedCells = Math.max(cellsCount, maxConnectedCells);
         }
-      } 
+      }
     }
     return maxConnectedCells;
   }
 
-  public static int getConnectedCountsRegion(int[] array, int row, int column) {
+  public static int getConnectedCountsRegion(int[][] array, int row, int column) {
 
-    int connectCount = 0;
+    if (row < 0 || column < 0 || row >= array.length || column >= array[row].length) {
+      return 0;
+    }
+
+    if (array[row][column] == 0) {
+      return 0;
+    }
+
+    int connectCount = 1;
     // We have visited this node, we can mark it as 0
     array[row][column] = 0;
 
     // check if there are any connected 1s adjacent to the element
-    for(int r=row-1; r<row+1; r++) {
-      for(int c=column-1; c<column+1; column++) {
-        if(row!=1 && column!=c) {
-          connectCount += getConnectedCountsRegion(array, r, c);
-        }   
+    for (int r = row - 1; r <= row + 1; r++) {
+      for (int c = column - 1; c <= column + 1; c++) {
+        if (row == r && column == c) {
+          continue;
+        }
+        connectCount += getConnectedCountsRegion(array, r, c);
       }
     }
     return connectCount;
   }
 
-  
+
+  public static void main(String[] args) {
+    int[][] array = {
+        {1, 1, 0, 0, 0},
+        {0, 1, 1, 0, 0},
+        {0, 0, 1, 0, 1},
+        {1, 0, 0, 0, 1},
+        {0, 1, 0, 1, 1},
+        {0, 1, 0, 1, 1},
+        {0, 1, 0, 1, 1}
+    };
+
+    System.out.println(connectedCell(array));
+
+  }
+
+
 }
