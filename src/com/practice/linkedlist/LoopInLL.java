e437d44468d2e38e545103019cce8d2a3acc0513
pjjmaster <pavanjoshi029@gmail.com>
2022-04-10 20:38:18 +0530

    Update LoopInLL.java

--- src/com/practice/linkedlist/LoopInLL.java
+++ src/com/practice/linkedlist/LoopInLL.java
@@ -2,9 +2,9 @@ package com.practice.linkedlist;
 
 public class LoopInLL {
 
-  Node head;
+ Node head;
   
- static class Node {
+ static class ListNode {
 
   int data;
   Node next;
@@ -12,16 +12,16 @@ public class LoopInLL {
   Node(int data) {
     this.data = data;
   }
-}
+ }
   
-    public boolean hasCycle(ListNode head) {
-    
+  public boolean hasCycle(ListNode head) {
+
     if(head==null) {
         return false;
     }    
     ListNode slowPtr = head;
     ListNode fastPtr = head;
-    
+
     while(fastPtr.next!=null && fastPtr.next.next!=null) {
       fastPtr = fastPtr.next.next;
       slowPtr = slowPtr.next;
@@ -29,14 +29,40 @@ public class LoopInLL {
         return true;
       }
     }
-    
+
     return false;  
-    }
+  }
 
         
+  
+  public Node getFirstNodeOfLoop() {
+    if (head == null) {
+      return null;
+    }
 
-  
-  
-  
-  
+    Node slowPtr = head;
+    Node fastPtr = head;
+    boolean isLoopPresent = false;
+
+    while (fastPtr.next != null && fastPtr.next.next != null) {
+      fastPtr = fastPtr.next.next;
+      slowPtr = slowPtr.next;
+      if (fastPtr.equals(slowPtr)) {
+        isLoopPresent = true;
+        break;
+      }
+    }
+    // Once we find the loop, we move initialize slowPtr head and incerement both of them one
+    if (isLoopPresent) {
+      slowPtr = head;
+      while (slowPtr != fastPtr) {
+        slowPtr = slowPtr.next;
+        fastPtr = fastPtr.next;
+      }
+      return slowPtr;
+    } else {
+      return null;
+    }
+  }
+
 }
