313e45e0c71f44031924edc994d8ed50198c4be8
pjjmaster <pavanjoshi029@gmail.com>
2022-04-19 09:30:42 +0530

    Update ThreeSum.java

--- src/com/practice/google/ThreeSum.java
+++ src/com/practice/google/ThreeSum.java
@@ -82,6 +82,46 @@ public class ThreeSum {
         }
         
     }
+
+    /*
+    Approach 3: "No-Sort"
+What if you cannot modify the input array, and you want to avoid copying it due to memory constraints?
+We can adapt the hashset approach above to work for an unsorted array. We can put a combination of three values into a hashset to avoid duplicates. 
+Values in a combination should be ordered (e.g. ascending). Otherwise, we can have results with the same values in the different positions.
+
+Algorithm
+The algorithm is similar to the hashset approach above. We just need to add few optimizations so that it works efficiently for repeated values:
+
+Use another hashset dups to skip duplicates in the outer loop.
+Without this optimization, the submission will time out for the test case with 3,000 zeroes. This case is handled naturally when the array is sorted.
+Instead of re-populating a hashset every time in the inner loop, we can use a hashmap and populate it once. 
+    Values in the hashmap will indicate whether we have encountered that element in the current iteration. When we process nums[j] in the inner loop, we set its hashmap value to i. This indicates that we can now use nums[j] as a complement for nums[i].
+This is more like a trick to compensate for container overheads. The effect varies by language, e.g. for C++ it cuts the runtime in half. Without this trick the submission may time out.
+ */
+    
+    public List<List<Integer>> threeSum(int[] nums) {
+        
+        Set<List<Integer>> resultSet = new HashSet<>();
+        Set<Integer> visited = new HashSet<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        for(int i = 0; i < nums.length; i++) {
+            if(visited.add(nums[i])) {
+                for(int j = i + 1; j < nums.length; j++) {
+                    int complement = -nums[i] - nums[j];
+                    if(map.containsKey(complement) && map.get(complement) == i) {
+                        List<Integer> triplet = Arrays.asList(nums[i], nums[j], complement);
+                        Collections.sort(triplet);
+                        resultSet.add(triplet);
+                    }
+                    map.put(nums[j], i);
+                }    
+            }
+        }
+        return new ArrayList(resultSet);
+    }
+    
+    
     
     
 
