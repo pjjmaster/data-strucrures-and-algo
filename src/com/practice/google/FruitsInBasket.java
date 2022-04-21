6ad31223124e963ac1eb668ff4873bef784d916f
pjjmaster <pavanjoshi029@gmail.com>
2022-04-21 20:39:48 +0530

    Update FruitsInBasket.java

--- src/com/practice/google/FruitsInBasket.java
+++ src/com/practice/google/FruitsInBasket.java
@@ -63,6 +63,35 @@ public class FruitsInBasket {
         return maxFruits;
     }
 
-
+   public int totalFruitsWorking(int[] fruits) {
+        
+        if(fruits == null && fruits.length == 0) {
+            return 0;
+        }
+        
+        Map<Integer, Integer> map = new HashMap<>();
+        int i= 0;
+        int j = 0;
+        int max = 1;
+        
+        while(j < fruits.length) {
+            
+            if(map.size() <= 2) {
+                map.put(fruits[j], j++);
+            }
+            
+            if(map.size() > 2) {
+                int min = fruits.length -1;
+                for(int value: map.values()) {
+                    min = Math.min(value, min);
+                }
+                i = min + 1;
+                map.remove(fruits[min]);               
+            }
+            max = Math.max(max, j - i);
+            
+        }
+        return max;
+    }
 
 }
