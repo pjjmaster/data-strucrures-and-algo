b53c8405bf5e86fd902b9f4429d3753672cb4197
pjjmaster <pavanjoshi029@gmail.com>
2022-05-01 09:05:03 +0530

    Create JewelsAndStones.java

---
+++ src/com/practice/google/JewelsAndStones.java
@@ -0,0 +1,40 @@
+package com.practice.google;
+
+/*
+You're given strings jewels representing the types of stones that are jewels, and stones representing the stones you have. Each character in stones is a type of stone you have. You want to know how many of the stones you have are also jewels.
+
+Letters are case sensitive, so "a" is considered a different type of stone from "A".
+
+ 
+
+Example 1:
+
+Input: jewels = "aA", stones = "aAAbbbb"
+Output: 3
+Example 2:
+
+Input: jewels = "z", stones = "ZZ"
+Output: 0
+*/
+
+public class JewelsAndStones {
+      public int numJewelsInStones(String jewels, String stones) {
+        if(jewels.length() == 0) {
+            return 0;
+        }
+        Set<Character> jewelsSet = new HashSet<>();
+        
+        for(char jewel : jewels.toCharArray()) {
+            jewelsSet.add(jewel);    
+        }
+        
+        int numJewels = 0;
+        for(char stone: stones.toCharArray()) {
+            if(jewelsSet.contains(stone)) {
+                numJewels++;
+            }
+        }
+        return numJewels;
+    }
+
+}
