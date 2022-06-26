f415d9b15f26307c5eef6e81d82f228f6afaeb41
pjjmaster <pavanjoshi029@gmail.com>
2022-06-26 12:06:36 +0530

    Update SmsSplit.java

--- src/com/practice/google/SmsSplit.java
+++ src/com/practice/google/SmsSplit.java
@@ -1,6 +1,72 @@
 package com.practice.google;
 
+/**
+Instructions
+
+Given an input string of arbitrary length, output SMS-compliant segments with suffixes.
+
+A SMS-compliant segment is of length 160 characters or less.
+Do not generate segments if the input fits in a single message.
+A segment suffix looks like "(1/5)" for the first of five segments.
+The segment content and suffix must both fit in the segment.
+You must complete the function/method stub to return an array of message segments.
+
+Input Constraints
+
+Inputs will only consist of A-Z, a-z, spaces ( ), commas (,) and periods (.)
+Your implementation can expect a maximum of 9 segments per input.
+
+Extra Credit Case
+
+The third test case has an additional output constraint: do not split words between segments! You should be able to iterate on your existing implementation.
+
+Words will be delimited by a single space. Do not split on any other punctuation. You do not need to account for "unbreakable" i.e extremely long words in the input.
+Spaces between words should be preserved in the first segment unless that violates the SMS length constraint, in which case the space should be included in the next segment.
+
+**/
+
 public class SmsSplit {
+  
+    //Working solution
+      public static List<String> segments(String message) {
+        List<String> result = new ArrayList<>();
+        if (message.length() < 160) {
+            result.add(message);
+            return result;
+        }
+        
+        int segmentSize = 154;
+        int start= 0;
+        int end = start + segmentSize;
+        
+        while(end < message.length()) {
+            
+            if(message.charAt(end) != ' ') {
+                
+                while(end >=start && message.charAt(end) != ' ' 
+                        && message.charAt(end + 1) != ' ') {
+                    end--;
+                }
+                
+            }
+            
+            result.add(message.substring(start, end + 1));
+            start = end + 1;
+            end = start + segmentSize; 
+        }
+         
+        result.add(message.substring(start, message.length()));
+        
+        for(int i = 0; i< result.size(); i++) {
+            result.set(i, result.get(i)+ "(" + (i+1) + "/" + result.size() + ")");
+        }
+        return result;
+        
+    }
+
+
+  
+  
   
     public static List<String> segments(String message) {
         List<String> segments = new ArrayList<>();
