6610a6d3ac4881be038d3bbd3c241e9041ff92af
pjjmaster <pavanjoshi029@gmail.com>
2024-09-06 18:34:27 +0530

    Update NumSubarrayProductLessThanK.java

--- src/com/practice/slidingwindow/NumSubarrayProductLessThanK.java
+++ src/com/practice/slidingwindow/NumSubarrayProductLessThanK.java
@@ -1,5 +1,23 @@
 public class NumSubarrayProductLessThanK {
+/**
+Given an array of integers nums and an integer k, return the number of contiguous subarrays where the product of all the elements in the subarray is strictly less than k.
+Example 1:
+Input: nums = [10,5,2,6], k = 100
+Output: 8
+Explanation: The 8 subarrays that have product less than 100 are:
+[10], [5], [2], [6], [10, 5], [5, 2], [2, 6], [5, 2, 6]
+Note that [10, 5, 2] is not included as the product of 100 is not strictly less than k.
+Example 2:
+Input: nums = [1,2,3], k = 0
+Output: 0
+Constraints:
+1 <= nums.length <= 3 * 104
+1 <= nums[i] <= 1000
+0 <= k <= 106
 
+  **/
+
+  
   public static int numSubarrayProductLessThanK(int[] nums, int k) {
     if (k <= 1) {
       return 0;
